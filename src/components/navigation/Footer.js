323e99693614599415cf2a386ea9b4284b932f8e
Christopher Young <christopheryoung@MacBook123.local>
2023-01-05 09:49:17 -0800

    added web icon

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 export default function Footer(props) {
   return (
     <div id="footer">
-      Footer
+      <a href="https://www.flaticon.com/free-icons/drink" title="drink icons" target="_blank" rel="noreferrer">Drink icons created by Freepik - Flaticon</a>
     </div>
   );
 };
